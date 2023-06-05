85d7dd65fc3192da247b41e0a906c94868fa2c1e
Sibasisbadatya <98338367+Sibasisbadatya@users.noreply.github.com>
2023-06-06 02:06:35 +0530

    .

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -79,7 +79,7 @@ const File = () => {
         formData.append('photo', newUser.photo);
         formData.append('name', newUser.name);
         formData.append('email', userEmail);
-        if (!formData.photo || !formData.name || !formData.email) {
+        if (!formData.photo || !formData.name) {
             toast.error("Enter all the details", toastOptions);
             return;
         }
